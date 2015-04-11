713aa3fdc8322df09fd0814a988e8ff706ce023f
Santosh Ghimire <santosh.ghimire33@gmail.com>
2015-04-11 15:34:10 +0545

    changes for hack_data.js

--- js/hack_data.js
+++ js/hack_data.js
@@ -1,4 +1,74 @@
 /*dummy datas*/
+var orgInfo = {
+    raksha : {
+        title : "Raksha Nepal",
+        tag : "Care and Support for sexually exploited girls, women and their children",
+        orgImg : "images/rakhsha.png",
+        mission : "To help rescue and rehabilitate sexually exploited women and girls so that they can rebuild their lives for a better future.",
+        points3 : ["Assists sexually exploited girls, women and their children with building their lives by providing shelter, legal assistance, counseling, schooling, health education, and skills training.",
+            " Create awareness about self-protection from sexual exploitation.",
+            "Lobby for effective implementation of national/international laws involving child labor."
+            ],
+        about : ["Raksha Nepal (RN) is a non-government, non-profitable social organization that works for sexually exploited girls, women, and their children.  It also empowers and provides care and support to them.",
+            "RN is run by women and girls who have been sexually exploited.  Because of this, the organization is dedicated to those women and girls who have landed knowingly and unknowingly in the dance bars, massage parlors, and cabin restaurants, where they are sexually exploited by the owners as well as by the customers. RN is committed to making these women and girls aware of how to protect themselves in their current roles.  RN also empowers them to build their capacity to identify alternative livelihood opportunities to start safe and respectable lives.",
+            "RN believes that if women are confident and education the entire family and society benefits from it."
+            ],
+        orgGeneralInfo : {
+            estd : 2004,
+            gpoBox : 24866,
+            address : "Kathmandu, Nepal",
+            phone : "+977-1-4437552",
+            fax : "+977-1-4024100",
+            web : "www.rakshanepal.org",
+            email : "infor@rakshanepal.org"
+        }
+
+    },
+    shakti : {
+        title : "Shakti Samuha",
+        tag : "Care and Support for sexually exploited girls, women and their children",
+        orgImg : "images/shakti.png",
+        mission : "To help establish a progressive society, devoid of trafficking and other kinds of violence against women.",
+        points3 : ["Known as the first organization of trafficking survivors in the world.",
+            " Shakti Samuha means Power Group, and founded by 15 trafficking survivors.",
+            "The founder, Charimaya Tamang, received the “Hero Acting to End Modern Day Slavery Award” in 2011 from Hillary Clinton. "
+            ],
+        about : ["Shakti Samuha is known as the first organization of trafficking survivors in the world. Their mission is to ensure women of trafficking survivors and children at risk of trafficking will be organized, aware, empowered and contribute in the campaign against human trafficking.",
+            "It was started in 1996, when the Indian government rescued around 500 Nepali, Indian and Bengalis women in the red light areas. 15 of the women women united together to convert their tears into the power and to fight against human trafficking in Nepal. They formed Shakti Samuha, and created a mission to organize and empower trafficking survivors."
+            ],
+        orgGeneralInfo : {
+            estd : 1996,
+            gpoBox : 19488,
+            address : "Kathmandu, Nepal",
+            phone : "+977-1-4478117",
+            web : "www.shaktisamuha.org.np",
+            email : "shaktisamuha.nepal@gmail.com"
+        }
+
+    },
+    cwin : {
+        title : "Child Workers in Nepal (CWIN)",
+        tag : "Care and Support for sexually exploited girls, women and their children",
+        orgImg : "images/cwin.png",
+        mission : "To be an advocate for child rights in Nepal in regard to child labor, street children, child marriage, bonded labor, trafficking of children, children in conflict with laws, and commercial sexual exploitation of children.",
+        points3 : ["Serves as an advocate for child rights in Nepal by acting as the voice of the child.",
+            "Offers socialization, support, and rehabilitation programs for children."
+            ],
+        about : ["Child Workers in Nepal Concerned Centre (CWIN) is a pioneer organization in Nepal for the rights of the child and against child labor exploitation. CWIN is an advocate organization for the child's rights with focus on children living and working under the most difficult circumstances. CWIN's main areas of concern are child labor, street children, child marriage, bonded labor, trafficking of children, children in conflict with laws and commercial-sexual exploitation of children.",
+            "As a watchdog in the field of child rights in the country, CWIN acts as a voice of children through lobbying, campaign and pressure to the government to protect and promote children's rights in the country, and to end all kinds of exploitation, abuse and discrimination against children.",
+            "As a concerned organization to the children at risk, CWIN has also been undertaking a number of socialization, support and rehabilitation programs."
+            ],
+        orgGeneralInfo : {
+            estd : 1987,
+            address : "Ravi Bhawan, Kathmandu, Nepal",
+            phone : "+977-1-4282255, +977-1-4278064",
+            web : "http://www.cwin.org.np",
+            email : "cwin@mos.com.np, sumni100@gmail.com, cwinnraic@gmail.com"
+        }
+
+    }        
+
+}
     var organizations = [
         {
             name: "Shakti Samuha",
@@ -55,48 +125,38 @@
         {vars : "a", datas : "Less than a day"},
         {vars : "b", datas : "Less than a week"},
         {vars : "c", datas : "More than a month"},
-        {vars : "d", datas : "More than a 3 month"}
+        {vars : "d", datas : "More than 3 months"}
     ]
     var projectAll = [
   {
-            title : "The title3",
-            location : "location",
-            picture : "images/project_img.jpg",
-            description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit minus facilis itaque, similique dolorum? Ipsa quaerat neque aliquid corporis modi alias, totam pariatur aut ab autem ut rem ducimus voluptatibus!",
-            projectType : "teaching",
-            ngoName : "ewwwwweeew sdfd",
-            category : "smth like",
-            timeSpanVar : "a",
-            url : "orgprofile.html?smth"
-
       title: "Tutor trafficking survivors in English studies",
       description: "A volunteer is needed to tutor trafficking survivors with their English studies for 2 hours/week. This opportunity is a 2 month commitment.",
       no_of_hours: "16-20 hours",
-      time_frame: "More than a month",
+      timeSpanVar: "c",
       starting_date: "2015-05-01",
       requirements: "Fluent English speaker and Female",
       purpose: "Build mentoring relationship to coach girls. Support education efforts to rehabilitate and empower at-risk youth. Build English skills.",
       projectType: "Tutoring",
-      ngoName: 2,
+      ngoName: "Raksha Nepal",
       picture: "image/1.png",
       category: "Internship",
-      location: "Kathmandu, Nepal"
+      location: "Kathmandu, Nepal",
+      url: "orgprofile.html?orgName=raksha"
   },
   {
-    
-    
       title: "Build event page for award-winning nonprofit’s 20th anniversary event",
       description: "Design and create Shakti Samuha’s event page to help them raise awareness and funds for their 20th year birthday party! This opportunity is for 2 weeks.",
       no_of_hours: "20-25 hours",
-      time_frame: "More than a month",
+      timeSpanVar: "c",
       starting_date: "2015-12-01",
       requirements: "Proficient at front-end design. Web development skills.",
       purpose: "Build your portfolio with impactful design projects. Support an award-winning nonprofit with their biggest event. Help an awesome nonprofit to raise money!",
       projectType: "Social Media",
-      ngoName: 1,
+      ngoName: "Shakti Samuha",
       picture: "image/2.png",
       category: "Volunteer",
-      location: "Kathmandu, Nepal"
+      location: "Kathmandu, Nepal",
+      url: "orgprofile.html?orgName=shakti"
   },
   {
     
@@ -104,15 +164,16 @@
       title: "Volunteer to teach computer skills",
       description: "A volunteer is required to teach some basic computer skills to the trafficked victims kids.",
       no_of_hours: "30-35 hours",
-      time_frame: "More than three months",
+      timeSpanVar: "d",
       starting_date: "2015-12-21",
       requirements: "Proficient at basic computer skills, email and internet.",
       purpose: "Teach the kids on how to use some basic computer tools like word, spreadsheet, presentation, email and social media.",
       projectType: "Tutoring",
-      ngoName: 3,
+      ngoName: "Child Workers in Nepal (CWIN)",
       picture: "image/3.png",
       category: "Internship",
-      location: "Ravi Bhawan, Kathmandu, Nepal"
+      location: "Ravi Bhawan, Kathmandu, Nepal",
+      url: "orgprofile.html?orgName=cwin"
   },  
   {
     
@@ -120,15 +181,16 @@
       title: "Lead social media strategy for award-winning nonprofit",
       description: "We need a social media rockstar to help us workout the social media strategy and online presence.",
       no_of_hours: "10-15 hours",
-      time_frame: "Less than a week",
+      timeSpanVar: "b",
       starting_date: "2015-10-21",
       requirements: "Proficient at social media use, strategic skills in making a unique social media presence.",
       purpose: "Help our organization spread the good works that we are doing and connect us with doners and supportive masses.",
       projectType: "Social Media",
-      ngoName: 1,
+      ngoName: "Shakti Samuha",
       picture: "image/4.png",
       category: "Internship",
-      location: "Kathmandu, Nepal"
+      location: "Kathmandu, Nepal",
+    url: "orgprofile.html?orgName=shakti"
   },  
   {
     
@@ -136,15 +198,16 @@
       title: "Support women’s shelter as Admin Assistant",
       description: "We need someone to help us manage and organize the women's shelter.",
       no_of_hours: "60-65 hours",
-      time_frame: "More than three month",
+      timeSpanVar: "d",
       starting_date: "2015-12-01",
       requirements: "Proficient at communication and management skills.",
       purpose: "Help manage the women’s shelter for good.",
       projectType: "Admin Assistant",
-      ngoName: 2,
+      ngoName: "Raksha Nepal",
       picture: "image/5.png",
       category: "Internship",
-      location: "Kathmandu, Nepal"
+      location: "Kathmandu, Nepal",
+    url: "orgprofile.html?orgName=raksha"
   },    
   {
     
@@ -152,59 +215,15 @@
       title: "Plan festival for nonprofit organizations",
       description: "We need an event manager to plan and organize our various kinds of festivals within the organization.",
       no_of_hours: "20 to 25 hours",
-      time_frame: "More than three months",
+      timeSpanVar: "d",
       starting_date: "2015-11-01",
       requirements: "Proficient at event management",
       purpose: "Better celebration at festivals",
       projectType: "Event Management",
-      ngoName: 3,
+      ngoName: "Child Workers in Nepal (CWIN)",
       picture: "image/6.png",
       category: "Internship",
-      location: "Kathmandu, Nepal"
+      location: "Kathmandu, Nepal",
+      url: "orgprofile.html?orgName=cwin"
   }
     ];
-    var projectAll = [
-        {
-            title : "The title3",
-            location : "location",
-            picture : "images/project_img.jpg",
-            description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit minus facilis itaque, similique dolorum? Ipsa quaerat neque aliquid corporis modi alias, totam pariatur aut ab autem ut rem ducimus voluptatibus!",
-            projectType : "teaching",
-            ngoName : "ewwwwweeew sdfd",
-            category : "smth like",
-            timeSpanVar : "a",
-            url : "orgprofile.html?smth"
-        },
-        {
-            title : "The title2",
-            location : "location",
-            picture : "images/project_img.jpg",
-            description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit minus facilis itaque, similique dolorum? Ipsa quaerat neque aliquid corporis modi alias, totam pariatur aut ab autem ut rem ducimus voluptatibus!",
-            projectType : "marketing",
-            ngoName : "ewwwwweeew sdfd",
-            category : "smth like",
-            timeSpanVar : "a",
-            url : "?smth"
-        },
-        {
-            title : "The title3",
-            location : "location",
-            picture : "images/project_img.jpg",
-            description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit minus facilis itaque, similique dolorum? Ipsa quaerat neque aliquid corporis modi alias, totam pariatur aut ab autem ut rem ducimus voluptatibus!",
-            projectType : "teaching",
-            ngoName : "ewwwwweeew sdfd",
-            category : "smth like",
-            timeSpanVar : "b",
-            url : "?smth"
-        },{
-            title : "The title4",
-            location : "location",
-            picture : "images/project_img.jpg",
-            description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit minus facilis itaque, similique dolorum? Ipsa quaerat neque aliquid corporis modi alias, totam pariatur aut ab autem ut rem ducimus voluptatibus!",
-            projectType : "fund raising",
-            ngoName : "ewwwwweeew sdfd",
-            category : "smth like",
-            timeSpanVar : "c",
-            url : "?smth"
-        }
-    ]
