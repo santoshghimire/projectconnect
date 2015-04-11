33334fd4de8dd08506dc81753d36ef772ec10dc2
Santosh Ghimire <santosh.ghimire33@gmail.com>
2015-04-11 14:59:12 +0545

    changes

---
+++ js/hack_data.js
@@ -0,0 +1,105 @@
+/*dummy datas*/
+    var organizations = [
+        {
+            name: "Shakti Samuha",
+            mission: "To help establish a progressive society, devoid of trafficking and other kinds of violence against women.",
+            description: "Shakti Samuha is known as the first organization of trafficking survivors in the world. Their mission is to ensure women of trafficking survivors and children at risk of trafficking will be organized, aware, empowered and contribute in the campaign against human trafficking. It was started in 1996, when the Indian government rescued around 500 Nepali, Indian and Bengalis women in the red light areas. 15 of the women women united together to convert their tears into the power and to fight against human trafficking in Nepal. They formed Shakti Samuha, and created a mission to organize and empower trafficking survivors.",
+            category: "Anti-human Trafficking",
+            location: "Kathmandu",
+            contact_number: "01-4478117",
+            year_established: "1996",
+            email: "shaktisamuha.nepal@gmail.com",
+            website: "http://shaktisamuha.org.np",
+            logo: "image/shakti.png",
+            picture_1: "",
+            picture_2: ""            
+        },
+        {
+            name: "Raksha Nepal",
+            mission: "To help rescue and rehabilitate sexually exploited women and girls so that they can rebuild their lives for a better future.",
+            description: "Raksha Nepal (RN) is a non-government, non-profitable social organization that works for sexually exploited girls, women, and their children. It also empowers and provides care and support to them. RN is run by women and girls who have been sexually exploited. Because of this, the organization is dedicated to those women and girls who have landed knowingly and unknowingly in the dance bars, massage parlors, and cabin restaurants, where they are sexually exploited by the owners as well as by the customers. RN is committed to making these women and girls aware of how to protect themselves in their current roles. RN also empowers them to build their capacity to identify alternative livelihood opportunities to start safe and respectable lives. RN believes that if women are confident and education the entire family and society benefits from it.",
+            category: "Anti-human Trafficking",
+            location: "Kathmandu",
+            contact_number: "01-4437552",
+            year_established: "2004",
+            email: "infor@rakshanepal.org",
+            website: "http://rakshanepal.org",
+            logo: "image/raksha.png",
+            picture_1: "",
+            picture_2: ""            
+        },
+        {
+            name: "Child Workers in Nepal (CWIN)",
+            mission: "To be an advocate for child rights in Nepal in regard to child labor, street children, child marriage, bonded labor, trafficking of children, children in conflict with laws, and commercial sexual exploitation of children.",
+            description: "Child Workers in Nepal Concerned Centre (CWIN) is a pioneer organization in Nepal for the rights of the child and against child labor exploitation. CWIN is an advocate organization for the child's rights with focus on children living and working under the most difficult circumstances. CWIN's main areas of concern are child labor, street children, child marriage, bonded labor, trafficking of children, children in conflict with laws and commercial-sexual exploitation of children. As a watchdog in the field of child rights in the country, CWIN acts as a voice of children through lobbying, campaign and pressure to the government to protect and promote children's rights in the country, and to end all kinds of exploitation, abuse and discrimination against children. As a concerned organization to the children at risk, CWIN has also been undertaking a number of socialization, support and rehabilitation programs.",
+            category: "Child Rights",
+            location: "Ravi Bhawan, Kathmandu",
+            contact_number: "01-4278064",
+            year_established: "1987",
+            email: "cwin@mos.com.np",
+            website: "http://www.cwin.org.np",
+            logo: "image/cwin.png",
+            picture_1: "",
+            picture_2: ""            
+        }
+    ];
+
+    var projectType = [
+        "Tutoring",
+        "Social Media",
+        "Admin Assistant",
+        "Event Management"
+    ];
+
+    var projectTime = [
+        {vars : "a", datas : "Less than a day"},
+        {vars : "b", datas : "Less than a week"},
+        {vars : "c", datas : "More than a month"},
+        {vars : "d", datas : "More than a 3 month"}
+    ]
+
+    var projectAll = [
+        {
+            title : "The title3",
+            location : "location",
+            picture : "images/project_img.jpg",
+            description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit minus facilis itaque, similique dolorum? Ipsa quaerat neque aliquid corporis modi alias, totam pariatur aut ab autem ut rem ducimus voluptatibus!",
+            projectType : "teaching",
+            ngoName : "ewwwwweeew sdfd",
+            category : "smth like",
+            timeSpanVar : "a",
+            url : "orgprofile.html?smth"
+        },
+        {
+            title : "The title2",
+            location : "location",
+            picture : "images/project_img.jpg",
+            description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit minus facilis itaque, similique dolorum? Ipsa quaerat neque aliquid corporis modi alias, totam pariatur aut ab autem ut rem ducimus voluptatibus!",
+            projectType : "marketing",
+            ngoName : "ewwwwweeew sdfd",
+            category : "smth like",
+            timeSpanVar : "a",
+            url : "?smth"
+        },
+        {
+            title : "The title3",
+            location : "location",
+            picture : "images/project_img.jpg",
+            description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit minus facilis itaque, similique dolorum? Ipsa quaerat neque aliquid corporis modi alias, totam pariatur aut ab autem ut rem ducimus voluptatibus!",
+            projectType : "teaching",
+            ngoName : "ewwwwweeew sdfd",
+            category : "smth like",
+            timeSpanVar : "b",
+            url : "?smth"
+        },{
+            title : "The title4",
+            location : "location",
+            picture : "images/project_img.jpg",
+            description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit minus facilis itaque, similique dolorum? Ipsa quaerat neque aliquid corporis modi alias, totam pariatur aut ab autem ut rem ducimus voluptatibus!",
+            projectType : "fund raising",
+            ngoName : "ewwwwweeew sdfd",
+            category : "smth like",
+            timeSpanVar : "c",
+            url : "?smth"
+        }
+    ]
